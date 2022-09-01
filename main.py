cd9800de36f9d126b342badc1ee4178f1cc14827
Bilqis Hafsah <bilqis.hafsah.ababiel@mail.ugm.ac.id>
2022-09-01 23:44:39 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 10, 100)
+y = 2 * x
+
+plt.plot(x,y)
---
+++ plottingAndVisualization.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from math import e
+
+t = np.linspace(0, 0.1, 100)
+print(e)
+xt = 3* e ^
